588d78b48e62bd6a0b505ed0667fb663f911753a
RyanONeill1970 <r@ryanoneill.com>
2018-11-14 19:28:05 +0000

    Add .Net Core dependency injection so can be used from other projects easily.

---
+++ GeneGenie.DataQuality.Tests/IntegrationTests/DependencyRegistration.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="DependencyRegistration.cs" company="GeneGenie.com">
+// Copyright (c) GeneGenie.com. All Rights Reserved.
+// Licensed under the GNU Affero General Public License v3.0. See LICENSE in the project root for license information.
+// </copyright>
+// <author> Copyright (C) 2018 Ryan O'Neill r@genegenie.com </author>
+
+namespace GeneGenie.DataQuality.Tests.IntegrationTests
+{
+    using GeneGenie.DataQuality.ExtensionMethods;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    /// <summary>
+    /// Tests to ensure the library can be registered and consumed from an external project.
+    /// </summary>
+    public class DependencyRegistration
+    {
+        /// <summary>
+        /// Ensures every year in the census enum has a date.
+        /// </summary>
+        [Fact]
+        public void Dependency_injection_is_registered_and_classes_can_be_resolved()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddDataQuality()
+                .BuildServiceProvider();
+
+            var addressQualityChecker = serviceProvider.GetRequiredService<AddressQualityChecker>();
+
+            Assert.NotNull(addressQualityChecker);
+        }
+    }
+}
