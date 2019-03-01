5045853e59395b301c4b8c1abd7a3255db850e21
RyanONeill1970 <r@ryanoneill.com>
2019-03-01 12:16:36 +0000

    SonarQube: Create temp directory if it does not exist.

--- SonarQube.bat
+++ SonarQube.bat
@@ -1,6 +1,7 @@
+mkdir .sonarqube
 dotnet tool install --global dotnet-sonarscanner
 dotnet test GeneGenie.DataQuality.Tests\GeneGenie.DataQuality.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="%CD%\opencover.xml"
 dotnet build-server shutdown
-dotnet sonarscanner begin /k:"GeneGenie.DataQuality" /d:sonar.organization="thegenegenieproject" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=%SonarQubeApiKey% /d:sonar.cs.opencover.reportsPaths="%CD%\opencover.xml"
+dotnet sonarscanner begin /k:"GeneGenie.DataQuality" /o:rganization="thegenegenieproject" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=%SonarQubeApiKey% /d:sonar.cs.opencover.reportsPaths="%CD%\opencover.xml"
 dotnet build
 dotnet sonarscanner end /d:sonar.login=%SonarQubeApiKey%
