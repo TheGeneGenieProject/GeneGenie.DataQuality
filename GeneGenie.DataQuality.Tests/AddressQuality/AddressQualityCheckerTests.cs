1f2a80a371b5faa3da4c788fd99e152aa6c91b84
Ryan O'Neill <r@ryanoneill.com>
2022-11-20 15:56:59 +0000

    Simplify address quality tests more.

--- GeneGenie.DataQuality.Tests/AddressQuality/AddressQualityCheckerTests.cs
+++ GeneGenie.DataQuality.Tests/AddressQuality/AddressQualityCheckerTests.cs
@@ -16,14 +16,9 @@ namespace GeneGenie.DataQuality.Tests.AddressQuality
     /// </summary>
     public class AddressQualityCheckerTests
     {
-        private readonly AddressQualityChecker addressQualityChecker;
-
-        public AddressQualityCheckerTests()
-        {
-            var dateParser = new DateParser();
-            addressQualityChecker = new AddressQualityChecker(dateParser);
-        }
-
+        /// <summary>
+        /// Gets test data for checking the output of <see cref="AddressQualityChecker(DateParser)"/>.
+        /// </summary>
         public static IEnumerable<object[]> AddressQualityData =>
             new List<object[]>
             {
@@ -52,10 +47,17 @@ namespace GeneGenie.DataQuality.Tests.AddressQuality
                 new object[] { "another four part name", AddressQualityStatus.OK },
             };
 
+        /// <summary>
+        /// Asserts that <see cref="AddressQualityChecker(DateParser)"/> handles know bad / good data.
+        /// </summary>
+        /// <param name="source">The text to validate.</param>
+        /// <param name="expected">The expected quality status of the text after validating.</param>
         [Theory]
         [MemberData(nameof(AddressQualityData))]
         public void Address_quality_can_be_calculated_correctly(string source, AddressQualityStatus expected)
         {
+            var addressQualityChecker = new AddressQualityChecker(new DateParser());
+
             var status = addressQualityChecker.StatusGuessFromSourceQuality(source);
 
             Assert.Equal(expected, status);
